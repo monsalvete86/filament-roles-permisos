fac15a7029c878b762110c3abeeb1264e7211e8f
monsalvete86 <ccmonpan@hotmail.com>
2023-09-19 02:18:54 -0500

    first commit

---
+++ app/Filament/Resources/UserResource/Pages/ListUsers.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Filament\Resources\UserResource\Pages;
+
+use App\Filament\Resources\UserResource;
+use Filament\Actions;
+use Filament\Resources\Pages\ListRecords;
+
+class ListUsers extends ListRecords
+{
+    protected static string $resource = UserResource::class;
+
+    protected function getHeaderActions(): array
+    {
+        if (auth()->user()->can('Crear roles')){
+            return [
+                Actions\CreateAction::make(),
+            ];
+        } else { return []; }
+    }
+}
